9b34cd1f66e94963665c7a8c76483178130b01d4
johndoeadams <johndoeadams@gmail.com>
2019-01-12 15:59:39 -0500

    update rows and database

--- assets/js/run.js
+++ assets/js/run.js
@@ -17,9 +17,11 @@ var train={
   frequency:"15",
   first_train:"00:00"
 }
-var trainKey;
 
+$("#delete").click(function(){
+  console.log("Delete Row");
 
+});
 // update page based on items in the database
 $("#submit").click(function(){
   event.preventDefault();
@@ -34,61 +36,24 @@ $("#submit").click(function(){
   train.destination=destination;
   train.frequency=frequency;
   train.first_train=firstTrain;
-  console.log("TRAIN OBJECT BEFORE child_added",train);
   database.ref().push(train);
-  console.log("TRAIN OBJECT AFTER child_added",train);
 
   // resetTrain();
 });
 
+$(".delete").click(function(){
+  console.log("REMOVE ROW");
+});
+
 $("#clear").click(function(){
   //delete all rows
+  console.log("CLEAR ALL ROWS");
+  $("tbody").html("");
+  database.ref().set(null);
 });
-
-$("#delete").click(function(){
-  //delete row
-  this.parent().empty();
-});
-
-function buildTable(name,destination,frequency,firstTrain){
-  var newRow = $("<tr>").append(
-    $("<td>").text(name),
-    $("<td>").text(destination),
-    $("<td>").text(frequency),
-    $("<td>").text(firstTrain),
-    //calculate time left
-    $("<td>").text(function(){
-      var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-      // console.log("firstTrainConverted: ",firstTrainConverted);
-      var currentTime = moment();
-      // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-      var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
-      // console.log("DIFFERENCE IN TIME: " + diffTime);
-      var tRemainder = diffTime % frequency;
-      // console.log("tRemainder: ",tRemainder);
-      // Minute Until Train
-      var tMinutesTillTrain = frequency - tRemainder;
-      // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-      // Next Train
-      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-      // console.log("first_train TIME: " + moment(nextTrain).format("hh:mm"));
-      newRow.addClass("tableRow");
-      return tMinutesTillTrain;
-    })
-  );
-  $("#trainTable").append(newRow);
-}
 
 database.ref().on("child_removed",function(childSnapshot){
-  var name=childSnapshot.val().name;
-  var destination=childSnapshot.val().destination;
-  var firstTrain=childSnapshot.val().first_train
-  var frequency=childSnapshot.val().frequency
-  buildTable(name,destination,frequency, firstTrain);
-  $("#notificationHere").append($("<h4 id='removeMessage'>ITEM REMOVED</h4>"));
-  var windowTimeout = setTimeout(function(){
-    $("#successMessage").remove();
-  }, 2000);
+
 });
 
 //collect change from the database and add a row to the table
@@ -103,15 +68,55 @@ database.ref().on("child_added", function(childSnapshot) {
   //current key
   var key= childSnapshot.key;
 
-buildTable(name,destination,frequency, firstTrain);
-
+  if(false){
+    //remove item if already in the database
+    // $("#notificationHere").append($("<h6 id='errorMessage'>ITEM ALREADY EXISTS</h6>"));
+    // var windowTimeout = setTimeout(function(){
+    //     $("#errorMessage").remove();
+    //   }, 2000);
+    //    database.ref(key).remove();
+    //   console.log("Duplicate detected: ", train, key);
+        // resetTrain();
+  }
+  else {
     //add item
     $("#notificationHere").append($("<h5 id='successMessage'>ITEM ADDED</h5>"));
     var windowTimeout = setTimeout(function(){
       $("#successMessage").remove();
     }, 2000);
+    var td=$("<td>DELETE</td>");
 
+    var newRow = $("<tr>").append(
+      $("<td>").text(name),
+      $("<td>").text(destination),
+      $("<td>").text(frequency),
+      $("<td>").text(firstTrain),
+      //calculate time left
+      $("<td>").text(function(){
+        var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+        // console.log("firstTrainConverted: ",firstTrainConverted);
+        var currentTime = moment();
+        // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+        // console.log("DIFFERENCE IN TIME: " + diffTime);
+        var tRemainder = diffTime % frequency;
+        // console.log("tRemainder: ",tRemainder);
+        // Minute Until Train
+        var tMinutesTillTrain = frequency - tRemainder;
+        // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        // console.log("first_train TIME: " + moment(nextTrain).format("hh:mm"));
+        return tMinutesTillTrain;
+      }),$("<td id="+key+" class="+"delete"+" onclick="+"$(this).parent().remove()>DELETE</td>")
+
+    );
+    // td.attr("data-key",key);
+     // newRow.attr("data-name",key);
+    $("#trainTable").append(newRow);
     // resetTrain();
+  }
+
 
 });
 
